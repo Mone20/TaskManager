935ef1facb77d64b7983721e6188a10837a340cd
ilya9985 <ilyavragnov99@gmail.ru>
2019-12-18 19:24:59 +0400

    Windows

--- controller/MainWindow.java
+++ controller/MainWindow.java
@@ -1,10 +1,15 @@
 package controller;
 
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.awt.event.ActionEvent;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -19,7 +24,7 @@ public class MainWindow implements Initializable {
 
     }
 
-    public void actionButtonPressed(javafx.event.ActionEvent actionEvent) {
+    public void actionButtonPressed(javafx.event.ActionEvent actionEvent) throws IOException {
         Object sourse = actionEvent.getSource();
         if(!(sourse instanceof Button)) return;
 
@@ -27,7 +32,16 @@ public class MainWindow implements Initializable {
         String title = "";
         switch (clickButton.getId()){
             case "btnAddTask":
-
+                Stage newStage = new Stage();
+                FXMLLoader newLoader = new FXMLLoader();
+                newStage.setTitle("AddTasks");
+                URL xmlUrl = getClass().getResource("AddTask.fxml");
+                newLoader.setLocation(xmlUrl);
+                Parent newRoot = newLoader.load();
+                Scene newScene = new Scene(newRoot);
+                newStage.setScene(newScene);
+                newStage.initModality(Modality.APPLICATION_MODAL);
+                newStage.show();
                 break;
             case "btnDelTask":
 
